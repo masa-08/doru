9dab58b94c0eb7500130eedcf76ce1b586435da0
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-12-20 08:37:55 +0900

    Change cli implementation based on daemon manager implementation and schema

--- doru/types.py
+++ doru/types.py
@@ -1,21 +1,6 @@
-from typing_extensions import Literal, TypedDict
+from typing_extensions import Literal
 
 Exchange = Literal["bitbank", "bitflyer"]
 Interval = Literal["1day", "1week", "1month"]
 Pair = Literal["BTC_JPY", "ETH_JPY"]  # TODO: 取引所ごとに異なるペアを提示する
 Status = Literal["Running", "Stopped"]
-
-
-class Task(TypedDict):
-    id: str
-    pair: Pair
-    amount: int
-    interval: Interval
-    exchange: Exchange
-    status: Status
-
-
-class Credential(TypedDict):
-    exchange: Exchange
-    key: str
-    secret: str
