36016aae24e1aef15e91f66598c7c4a51151af30
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2023-01-22 19:07:49 +0900

    Implement API router

---
+++ doru/api/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from doru.api.router import router
+from doru.manager.container import Container
+
+
+def create_app() -> FastAPI:
+    container = Container()
+    app = FastAPI()
+    setattr(app, "container", container)
+    app.include_router(router)
+    return app
